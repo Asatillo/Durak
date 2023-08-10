23a7e31c455c931ac211e9519944b7b5287dbfe4
Asatillo Akhmedov <asatillo.akhmedov@gmail.com>
2023-08-11 00:00:35 +0500

    implemented cards

--- card.py
+++ card.py
@@ -1,8 +1,19 @@
 class Card():
     
-    def __init__(self, _type, value):
+    signs = {'hearts': '♥', 'diamonds': '♦', 'spades': '♠', 'club': '♣'}
+
+    def __init__(self, _type: str, value: int, rank: str):
+        '''
+        _type: str - the type of the card (hearts, diamonds, spades, club)
+        value: int - the numeric value of the card (6, 7, 8, 9, 10, 11, 12, 13, 14)
+        rank: str - the rank of the card (6, 7, 8, 9, 10, J, Q, K, A)
+        '''
         self.type = _type
         self.value = value
+        self.rank = rank
 
     def get_name(self):
-        return f'% of %', self.value, self.type 
+        return str(self)
+    
+    def __str__(self):
+        return f'{Card.signs[self.type]}{self.rank} '
